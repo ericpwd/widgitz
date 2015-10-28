82064aa616506eb03e81495da639171c49dc15d2
Colin Pirillo <colinpir13@gmail.com>
2015-10-28 07:49:14 -0400

    Finalized basic math widget!
    Added the core functionality and look!

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,5 +14,23 @@ angular.module('starter.controllers', [])
             uses:0   
         }
     ];
+
+    $scope.basicmath={
+    	number1:null,
+    	number2:null,
+    	answer:null,
+    	add: function  (n1, n2) {
+    		$scope.basicmath.answer = parseFloat(n1) + parseFloat(n2);
+    	},
+    	multiply: function (n1, n2) {
+    		$scope.basicmath.answer = parseFloat(n1) * parseFloat(n2);
+    	},
+    	substract: function (n1, n2) {
+    		$scope.basicmath.answer = parseFloat(n1) - parseFloat(n2);
+    	},
+    	divide: function (n1, n2) {
+    		$scope.basicmath.answer = parseFloat(n1) / parseFloat(n2);
+    	}
+    }
     
 });
